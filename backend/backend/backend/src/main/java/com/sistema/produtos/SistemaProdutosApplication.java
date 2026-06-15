f38abf39ab56144f2b74b08277b0770b96e70bbb
JoaoVARsilva22 <joaovardasilva@gmail.com>
2026-06-15 18:55:53 -0300

    Create SistemaProdutosApplication.java

---
+++ backend/backend/backend/src/main/java/com/sistema/produtos/SistemaProdutosApplication.java
@@ -0,0 +1,6 @@
+spring.datasource.url=jdbc:postgresql://${DB_HOST:localhost}:5432/${DB_NAME:produtos_db}
+spring.datasource.username=${DB_USER:admin}
+spring.datasource.password=${DB_PASS:admin}
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
+server.port=8080
